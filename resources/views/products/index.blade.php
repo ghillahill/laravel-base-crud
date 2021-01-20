7e1f6fd2eff047ea279afabe2b9f82fc12faa443
Alberto <thebertsinfo@gmail.com>
2021-01-20 14:41:32 +0100

    test visualizzazione in pagina di un elemento del db

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,5 +5,16 @@
         <div class="row">
             <h1>Products</h1>
         </div>
+        <div class="row">
+            <div class="col-12">
+                <ul>
+                    @foreach ($products as $product)
+                        <li>
+                            <p>{{$product->name}}</p>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
     </div>
 @endsection
